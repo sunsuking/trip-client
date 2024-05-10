7c4fd7164bb06d2fdacd50b9f8e57cc03edd4315
sunsuking <sunsuking@gmail.com>
2024-05-10 13:34:57 +0900

    플랜 UI 생성

--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -1,40 +1,22 @@
 <script setup lang="ts">
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Search } from "lucide-vue-next";
-import { KakaoMap } from "vue3-kakao-maps";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { ref } from "vue";
+import { categoryRequest } from "@/api/trip";
 import TripPlanCard from "@/components/card/TripPlanCard.vue";
-import { useToast } from "@/components/ui/toast";
 import TripManageBox from "@/components/trip/TripManageBox.vue";
+import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { useQuery } from "@tanstack/vue-query";
+import { Search } from "lucide-vue-next";
+import { ref } from "vue";
+import { KakaoMap } from "vue3-kakao-maps";
 
 const cards = Array.from({ length: 10 }, (_, i) => i);
 
-const categories = [
-  "공연",
-  "음식",
-  "숙소",
-  "관광지",
-  "기타",
-  "쇼핑",
-  "카페",
-  "박물관",
-  "테마파크",
-  "레포츠",
-];
-
-const toast = useToast();
+const { data: categories, isLoading: isCategoryLoading } = useQuery({
+  queryKey: ["categories"],
+  queryFn: categoryRequest,
+});
 
 const categoryGroup = ref([]);
 </script>
@@ -70,15 +52,24 @@ const categoryGroup = ref([]);
               type="multiple"
               class="justify-start overflow-scroll scrollbar-hide"
               v-model="categoryGroup"
+              v-if="isCategoryLoading"
             >
               <ToggleGroupItem
                 variant="outline"
                 v-for="category in categories"
-                :key="category"
-                :value="category"
-                ><span class="text-nowrap w-12 h-5">{{ category }}</span>
+                :key="category.contentId"
+                :value="category.name"
+                ><span class="text-nowrap w-14 h-5">{{ category.korName }}</span>
               </ToggleGroupItem>
             </ToggleGroup>
+            <div v-else class="flex flex-row space-x-4">
+              <Skeleton
+                v-for="i in [1, 2, 3, 4, 5]"
+                :key="i"
+                class="w-56 h-9 rounded-md"
+              />
+            </div>
+
             <div
               class="flex flex-col max-h-[530px] my-6 space-y-2 grow overflow-y-scroll scrollbar-hide"
             >
