f15248d175a2a7e69f037d06e56feb10952ba111
kimyongsu <akys159357@naver.com>
2024-05-13 19:34:48 +0900

    브랜치 생성

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,51 +1,78 @@
-import { useAuthenticationStore } from "@/stores/authentication"
-import client, {type BaseResponse } from "@/api/client"
-import type { EmailConfirmForm, FindPasswordForm, JwtToken, ResetPasswordForm, SignInForm, SignUpForm } from "@/types/auth.type"
+import { useAuthenticationStore } from '@/stores/authentication'
+import client, { type BaseResponse } from '@/api/client'
+import type {
+  EmailConfirmForm,
+  FindPasswordForm,
+  JwtToken,
+  ResetPasswordForm,
+  SignInForm,
+  SignUpForm
+} from '@/types/auth.type'
 
 export const signInRequest = async (signIn: SignInForm) => {
-  const { data: { isSuccess, message, data: { accessToken, refreshToken } } } = await client.post<BaseResponse<JwtToken>>(`/auth/sign-in`, signIn)
+  const {
+    data: {
+      isSuccess,
+      message,
+      data: { accessToken, refreshToken }
+    }
+  } = await client.post<BaseResponse<JwtToken>>(`/auth/sign-in`, signIn)
   if (!isSuccess) throw new Error(message)
-  
-  sessionStorage.setItem("accessToken", accessToken)
-  sessionStorage.setItem("refreshToken", refreshToken)
-  
+
+  sessionStorage.setItem('accessToken', accessToken)
+  sessionStorage.setItem('refreshToken', refreshToken)
+
   const authentication = useAuthenticationStore()
   authentication.setAccessToken(accessToken)
 }
 
 export const signUpRequest = async (signUp: SignUpForm) => {
-  const { data: { isSuccess, message } } = await client.post<BaseResponse<void>>(`/auth/sign-up`, signUp)
+  const {
+    data: { isSuccess, message }
+  } = await client.post<BaseResponse<void>>(`/auth/sign-up`, signUp)
   if (!isSuccess) throw new Error(message)
 }
 
 export const findPasswordRequest = async (findPassword: FindPasswordForm) => {
-  const { data: { isSuccess, message } } = await client.post<BaseResponse<void>>(`/auth/find-password`, findPassword)
+  const {
+    data: { isSuccess, message }
+  } = await client.post<BaseResponse<void>>(`/auth/find-password`, findPassword)
   if (!isSuccess) throw new Error(message)
 }
 
 export const resetPasswordRequest = async (resetPassword: ResetPasswordForm) => {
-  const { data: { isSuccess, message } } = await client.post<BaseResponse<void>>(`/auth/reset-password`, resetPassword)
+  const {
+    data: { isSuccess, message }
+  } = await client.post<BaseResponse<void>>(`/auth/reset-password`, resetPassword)
   if (!isSuccess) throw new Error(message)
 }
 
 export const emailConfirmRequest = async (confirm: EmailConfirmForm) => {
-  const { data: { isSuccess, message } } = await client.post<BaseResponse<void>>(`/auth/confirm`, confirm)
+  const {
+    data: { isSuccess, message }
+  } = await client.post<BaseResponse<void>>(`/auth/confirm`, confirm)
   if (!isSuccess) throw new Error(message)
 }
 
 export const signOutRequest = async () => {
   await client.delete<BaseResponse<void>>(`/auth/sign-out`)
-  sessionStorage.removeItem("accessToken")
-  sessionStorage.removeItem("refreshToken")
+  sessionStorage.removeItem('accessToken')
+  sessionStorage.removeItem('refreshToken')
 }
 
 export const refreshRequest = async () => {
-  const { data: { isSuccess, message, data: { accessToken, refreshToken } } } = await client.post<BaseResponse<JwtToken>>(`/auth/refresh`)
+  const {
+    data: {
+      isSuccess,
+      message,
+      data: { accessToken, refreshToken }
+    }
+  } = await client.post<BaseResponse<JwtToken>>(`/auth/refresh`)
   if (!isSuccess) throw new Error(message)
-  
-  sessionStorage.setItem("accessToken", accessToken)
-  sessionStorage.setItem("refreshToken", refreshToken)
-  
+
+  sessionStorage.setItem('accessToken', accessToken)
+  sessionStorage.setItem('refreshToken', refreshToken)
+
   const authentication = useAuthenticationStore()
   authentication.setAccessToken(accessToken)
 }
