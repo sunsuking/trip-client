471a494264b570e1d21e3576ce473779a233ac8d
sunsuking <sunsuking@gmail.com>
2024-05-23 10:29:04 +0900

    로고 추가

--- src/stores/authentication.ts
+++ src/stores/authentication.ts
@@ -8,7 +8,7 @@ export const useAuthenticationStore = defineStore('authentication', () => {
   const profile = ref<Profile | undefined>(undefined)
   const expireTime = computed<number | undefined>(() => {
     if (accessToken.value) {
-      return jwtDecode(accessToken.value).exp!! * 1000
+      return (jwtDecode(accessToken.value).exp!! * 1000) - (1000 * 60 * 3)
     }
     return undefined
   })
